5c6d7921b4c0e2f446df51105818f912940ce839
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2022-12-12 21:38:23 -0300

    feat: 🔥 validation of notifications

--- src/create-notifications-body.ts
+++ src/create-notifications-body.ts
@@ -1,9 +1,14 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
 
 export class CreateNotificationsBody {
   @IsNotEmpty()
+  @IsUUID()
   recipientId: string;
 
+  @IsNotEmpty()
+  @Length(3, 240)
   content: string;
+
+  @IsNotEmpty()
   category: string;
 }
