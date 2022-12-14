1a9f409807506e8667976620db42e6513fe6e2df
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2022-12-13 22:37:57 -0300

    chore: categorization of notification content test

--- src/app/entities/content.spec.ts
+++ src/app/entities/content.spec.ts
@@ -1,15 +1,17 @@
 import { Content } from './Content';
 
-test('it shold be able to crate a notification content', () => {
-  const content = new Content('Você recebeu uma notificação, parabéns!');
+describe('Notification Content', () => {
+  it('shold be able to crate a notification content', () => {
+    const content = new Content('Você recebeu uma notificação, parabéns!');
 
-  expect(content).toBeTruthy();
-});
+    expect(content).toBeTruthy();
+  });
 
-test('it shold not be able to crate a notification content with less than 3 characters', () => {
-  expect(() => new Content('ok')).toThrow();
-});
+  it('shold not be able to crate a notification content with less than 3 characters', () => {
+    expect(() => new Content('ok')).toThrow();
+  });
 
-test('it shold not be able to crate a notification content with more than 240 characters', () => {
-  expect(() => new Content('g'.repeat(241))).toThrow();
+  it('shold not be able to crate a notification content with more than 240 characters', () => {
+    expect(() => new Content('g'.repeat(241))).toThrow();
+  });
 });
