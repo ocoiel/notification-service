9aa31f2dd46f4f3b4742ef10bde2f7fc7dfa58e5
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2022-12-14 17:43:13 -0300

    chore: prettier fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,1 +1,11 @@
-import { Module } from '@nestjs/common';import { AppService } from './app.service';import { AppController } from './infra/app.controller';import { PrismaService } from './infra/primsa.service';@Module({  imports: [],  controllers: [AppController],  providers: [AppService, PrismaService],})export class AppModule {}
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AppController } from './infra/app.controller';
+import { PrismaService } from './infra/primsa.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
+})
+export class AppModule {}
